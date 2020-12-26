8647769ff51b647db4ed43fff60826964767c357
wa-r <nyuuuuun14@gmail.com>
2020-12-26 14:29:55 +0000

    Likeモデル追加

---
+++ app/models/user.rb
@@ -0,0 +1,15 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  attachment :profile_image, destroy: false
+
+  has_many :books
+  has_many :likes
+
+
+  validates :name, length: {maximum: 20, minimum: 2}, uniqueness: true
+  validates :introduction, length: {maximum: 50}
+end
